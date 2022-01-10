86ae5d82123889041cb370c28ecc652004ba61f4
Zer0SkiLL <retowidmer95@hotmail.com>
2022-01-10 12:18:10 +0100

    Updates

---
+++ precache-manifest.ad2483f797ce4dd6ec50c76096eb44d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db7f398edfce4161addff6eaad099668",
+    "url": "/monsters-browser/index.html"
+  },
+  {
+    "revision": "8f5ba16c140d66132ebb",
+    "url": "/monsters-browser/static/css/main.a9bd0eee.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/monsters-browser/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-browser/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f5ba16c140d66132ebb",
+    "url": "/monsters-browser/static/js/main.198deda5.chunk.js"
+  },
+  {
+    "revision": "72f58ca601e1e224842e",
+    "url": "/monsters-browser/static/js/runtime-main.97f18161.js"
+  }
+]);
